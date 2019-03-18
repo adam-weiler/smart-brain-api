d53f29a4141c48d60d5cdab2c876872ad43eea70
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-03-17 23:14:27 -0400

    Updating to Lecture 283.

--- server.js
+++ server.js
@@ -2,114 +2,47 @@ const express = require('express');
 const bodyParser = require('body-parser'); //To parse data sent from the browser through POST.
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smartbrain'
+  }
+});
+
+//console.log(db.select("*").from('users'));
+
+db.select("*").from('users').then(data => {
+	console.log(data)
+})
 
 const app = express();
-
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '456',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	],
-	login: [
-		{
-			id: '987',
-			hash: '',
-			email: 'john@gmail.com'
-		}
-	]
-}
 
 app.use(bodyParser.json());
 app.use(cors())
 
-app.get('/', (request, response) => {
-	//response.send('This is working!!');
-	response.send(database.users);
+app.get('/', (req, res) => {
+	//res.send('This is working!!');
+	res.send(database.users);
 })
 
-app.post('/signin', (request, response) => { //This is for the user signing in.
-	// bcrypt.compare("piano", '$2a$10$cfgEwHwR7YLyBrg5Ow9bMeM3uvkMYmcvvYxTQq/1ucVNp.i.z7ySW', function(err, result) {
-	// 	console.log("First guess is right.")		
-	// })
+//app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) }); //This is for the user signing in to account. //Basic
+app.post('/signin', signin.handleSignIn(db, bcrypt)); //This is for the user signing in to account. //Advanced
 
-	// bcrypt.compare("veggies", '$2a$10$cfgEwHwR7YLyBrg5Ow9bMeM3uvkMYmcvvYxTQq/1ucVNp.i.z7ySW', function(err, result) {
-	// 	console.log("Second guess is wrong.")	
-	// })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }); //This is for the user registering new account.
 
-	//Checks if the request from the body of browser is same as database.
-	if (request.body.email === database.users[0].email &&
-		request.body.password === database.users[0].password) {
-		response.json(database.users[0]);
-		//response.json("Success logging in!");
-	} else {
-		response.status(400).json('Error logging in.');
-	}
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }); //This shows the user their profile page.
 
-app.post('/register', (request, response) => { //This is for the user registering new account.
-	const { email, name, password } = request.body; //Use destructuring to get data from user.
-
-	// bcrypt.hash(password, null, null, function(err, hash) { //Bcrypt hashes the user's password.
-	// 	console.log(hash);
-	// })
-
-	database.users.push({ //Add user at end of "database".
-		id: '789',
-		name: name,
-		email: email,
-		entries: 0,
-		joined: new Date()
-	});
-	response.json(database.users[database.users.length-1]); //Last registered user.
-});
-
-app.get('/profile/:id', (request, response) => { //This shows the user their profile page.
-	const { id } = request.params; //Use destructuring to get data from user.
-	let found = false;
-
-	database.users.forEach(users => { //Loop through the database.
-		if (users.id === id) { //Check if current iteration id is same as data from user.
-			found = true;
-			//return response.json(users);
-			return res.json(database.users[0]);
-		}
-	});
-
-	if (!found) { //Loop is finished, user was not found.
-		response.status(404).json('No such user.');
-	}
-});
-
-app.put('/image', (request, response) => { //Add one to users rank.
-	const { id } = request.body; //Use destructuring to get data from user.
-	let found = false;
-
-	database.users.forEach(user => { //Loop through the database.
-		if (user.id === id) { //Check if current iteration id is same as data from user.
-			found = true;
-			user.entries++;
-			return response.json(user.entries);
-		}
-	});
-
-	if (!found) { //Loop is finished, user was not found.
-		response.status(404).json('No such user.');
-	}
-});
+app.put('/image', (req, res) => { image.handleImage(req, res, db) }); //Use destructuring to get data from user.
 
 app.listen(3000, ()=> {
 	console.log("It's running on port 3000!")
